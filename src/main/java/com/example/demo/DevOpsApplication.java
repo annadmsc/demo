32629867ae170981809eff758409bfc7e7fa0ae9
Anna Damasceno <99514379+annadmsc@users.noreply.github.com>
2023-08-28 22:50:10 +0000

    return teste ana

--- src/main/java/com/example/demo/DevOpsApplication.java
+++ src/main/java/com/example/demo/DevOpsApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DevOpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevOpsApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Teste ana";
+	}
+
 
 }
